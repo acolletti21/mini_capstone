49ef52ac44ce895a14fbbdedf41b421348480ef4
acolletti21 <ashleymcolletti@gmail.com>
2017-01-22 10:50:33 -0600

    added the 7 different routing options

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,9 +4,44 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def show
-    @product = Product.find_by(params[:id])
+
+  def new
   end
+
+  def create
+    @products = Products.new(
+                            name: params[:name],
+                            price: params[:price],
+                            image: params[:image],
+                            description: params[:description]
+                            )
+    @product.save
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.image = params[:image]
+    @product.description = params[:description]
+
+    @product.save
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+  end
+    
+
 end
 
 
